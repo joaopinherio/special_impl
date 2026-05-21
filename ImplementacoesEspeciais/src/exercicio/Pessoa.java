9f0b7a26d7ae6df35ae4a458b246e351189cc03c
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-05-21 16:01:13 +0000

    class Pessoa prot

--- ImplementacoesEspeciais/src/exercicio/Pessoa.java
+++ ImplementacoesEspeciais/src/exercicio/Pessoa.java
@@ -16,39 +16,39 @@ public class Pessoa {
         this.nome = nome;
     }
 
+    
     class Nascimento{
         public int dia;
         public int mes;
         public int ano;
-
+    
         public Nascimento(int dia, int mes, int ano){
             this.dia = dia;
             this.mes = mes;
             this.ano = ano;
         }
-
+    
         public int getDia(){
             return this.dia;
         }
-
+    
         public void setDia(int dia){
             this.dia = dia;
         }
-
+    
         public int getMes(){
             return this.mes;
         }
-
+    
         public void setMes(int mes){
             this.mes = mes;
         }
         public int getAno(){
             return this.ano;
         }
-
+    
         public void setAno(int ano){
             this.ano = ano;
         }
     }
-
 }
